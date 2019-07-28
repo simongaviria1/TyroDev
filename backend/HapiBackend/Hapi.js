b428ce8e80e1881bf2a2876709c1ca92dba8f0e8
Simon <simongaviria1@gmail.com>
2019-07-28 19:57:53 -0400

    added routes for requests

--- backend/HapiBackend/Hapi.js
+++ backend/HapiBackend/Hapi.js
@@ -1,26 +1,51 @@
-const Hapi = requires('@hapi/hapi')
+const Hapi = require('@hapi/hapi')
 
-const init = async () => { 
+const init = async () => {
 	const server = Hapi.server({
-	port : 3000, 
-	host : 'localhost'
-});
+		port: 3001,
+		host: 'localhost'
+	});
 
-server.route({
-	method: 'GET', 
-	path : '/', 
-	handler: (request, h) => {
-		return 'Hello World!'
-}
-}); 
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: (request, h) => {
+			return 'Hello World!'
+		}
+	});
 
-	await server.start(); 
-	console.log(`Server running on %s`, server.info.uri); 
+	server.route({
+		method: 'GET',
+		path: '/account/{username}',
+		handler: (request, h) => {
+			var accountMoch = {};
+			if (request.params.username === 'simon') {
+				accountMoch = {
+					'username': 'simon',
+					'password': '12345678'
+				}
+			}
+			console.log(h)
+			return accountMoch;
+		}
+	})
+
+	server.route({
+		method: 'POST',
+		path: '/account',
+		handler: (request, response) => {
+			console.log(response)
+			return request.payload;
+		}
+	})
+
+	await server.start();
+	console.log(`Server running on %s`, server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => { 
+process.on('unhandledRejection', (err) => {
 	console.log(err);
-	process.exit(1); 
+	process.exit(1);
 });
 
 init(); 
